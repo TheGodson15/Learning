576893a3a6516897e850cefb7bd0b6ec0e6d5735
Ankit Sarojkar <110610793+TheGodson15@users.noreply.github.com>
2024-02-28 09:07:30 +0000

    Practicing git Codespace

---
+++ Basics/01_variables.js
@@ -0,0 +1,9 @@
+// console.log("Mai to Papa hu! Is duniya ka papa")
+
+const accountId = 447928;
+var Name = "Ankit Sarojkar"
+let Age = 24
+
+console.log(accountId);
+console.log(Name);
+console.log(Age);
